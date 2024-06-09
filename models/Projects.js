726b45cab2b843fb025b221865d755cfc9d0843b
ToufiqAnim <toufiq9610@gmail.com>
2024-06-10 02:49:55 +0600

    code edited

--- models/Projects.js
+++ models/Projects.js
@@ -5,7 +5,7 @@ const projectSchema = new mongoose.Schema({
   title: String,
   description: String,
   technologies: String,
-  image: String,
+  image: Array,
   github: String,
   live: String,
 });
