96b365c5e49f74fb946fa130c4e086126ce34e80
ToufiqAnim <toufiq9610@gmail.com>
2024-06-09 22:47:00 +0600

    Portfolio get by id

--- routes/ProjectRoutes.js
+++ routes/ProjectRoutes.js
@@ -12,7 +12,17 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
-
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    if (project == null) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 // POST a new project
 router.post('/', async (req, res) => {
   const project = new Project(req.body);
